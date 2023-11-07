b6f89c07cba5561b0e3f764e322bca213af53217
nenyaa <139075287+nenyaa@users.noreply.github.com>
2023-11-07 23:06:32 +0100

    feat: read user functionality

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
+from data.data import users
+
+
+def show_users(users: list) -> None:
+    for item in user_generator(users):
+        print(item)
+
+
+def user_generator(userlist: list):
+    for user in userlist:
+        yield user
+
+
 def main() -> None:
-    pass
+    show_users(users)
 
 
 if __name__ == '__main__':
