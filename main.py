02b2cfcff9145e9ccb8247c4d48a630ad289f428
Marek Wyszyński <45630165+gitgeoman@users.noreply.github.com>
2023-11-08 21:20:33 +0100

    chore: this will be better at the beginning

--- main.py
+++ main.py
@@ -1,15 +1,9 @@
 from data.data import users
 
 
-def show_users(users: list) -> None:
-    for item in user_generator(users):
-        print(item)
-
-
-def user_generator(userlist: list):
-    for user in userlist:
-        yield user
-
+def show_users(users):
+    for user in users:
+        print(user)
 
 def main() -> None:
     show_users(users)
